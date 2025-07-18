dd7e006d824984dd32b9ee0fd9fd62cf42c1d9c3
Rainer Keller <rainer.keller@hs-esslingen.de>
2025-07-18 18:54:49 +0200

    Missed in last commit

--- 02_lcd_display/src/lcd_display.c
+++ 02_lcd_display/src/lcd_display.c
@@ -1,15 +1,15 @@
 #include "pico/stdlib.h"
-#include "lib/lcd_display"
+#include "lcd/lcd.h"
 
 const char black[3] = {0x00, 0x00, 0x00};
 const char white[3] = {0xff, 0xff, 0xff};
 
 int main() {
-    lcd_display_init(void);
+    lcd_init();
     while (true) {
-        lcd_display_bg(black);
+        lcd_circle(LCD_W2, LCD_H2, LCD_W/2, 0x00, 1, true);
         sleep_ms(500);
-        lcd_display_bg(white);
+        lcd_circle(LCD_W2, LCD_H2, LCD_W/2, 0xffff, 1, true);
         sleep_ms(500);
     }
 }
