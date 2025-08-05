2c521db3aca6536e3c9619e1a70a66f1192ff786
Rainer Keller <rainer.keller@hs-esslingen.de>
2025-08-05 22:07:56 +0200

    Add the LCD-test using simple LCD_1in28

--- 02_lcd_display/src/lcd_display.c
+++ 02_lcd_display/src/lcd_display.c
@@ -1,15 +1,39 @@
 #include "pico/stdlib.h"
-#include "lcd/lcd.h"
+#include "Config/DEV_Config.h"
+#include "lcd/LCD_1in28.h"
 
-const char black[3] = {0x00, 0x00, 0x00};
-const char white[3] = {0xff, 0xff, 0xff};
+#define BLACK          0x0000
+#define BLUE           0x001F
+#define GREEN          0x07E0
+#define CYAN           0x7FFF
+#define RED            0xF800
+#define MAGENTA        0xF81F
+#define WHITE          0xFFFF
+const int colors[] = {BLACK, BLUE, GREEN, CYAN, RED, MAGENTA, WHITE};
 
 int main() {
-    lcd_init();
+    uint16_t i = 0;
+    if (DEV_Module_Init() != 0)
+        return -1;
+
+    adc_init();
+    adc_gpio_init(29);
+    adc_select_input(3);
+    /* LCD Init */
+    printf("1.28inch LCD demo...\r\n");
+    LCD_1IN28_Init(HORIZONTAL);
+    LCD_1IN28_Clear(WHITE);
+    DEV_SET_PWM(60);
+
+    printf("Starting to rotate through colors\n");
     while (true) {
-        lcd_circle(LCD_W2, LCD_H2, LCD_W/2, 0x00, 1, true);
-        sleep_ms(500);
-        lcd_circle(LCD_W2, LCD_H2, LCD_W/2, 0xffff, 1, true);
-        sleep_ms(500);
+        LCD_1IN28_Clear(colors[i]);
+        sleep_ms(250);
+
+        i = (i+1) % (sizeof(colors)/sizeof(colors[0]));
+        printf("%d\n", i);
     }
+
+    /* Module Exit */
+    DEV_Module_Exit();
 }
