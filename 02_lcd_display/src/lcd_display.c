7063acc9de5068c470107785cfacd283f1f0068c
Rainer Keller <rainer.keller@hs-esslingen.de>
2025-09-12 11:17:00 +0200

    Cleanup example for LCD display

--- 02_lcd_display/src/lcd_display.c
+++ 02_lcd_display/src/lcd_display.c
@@ -2,23 +2,14 @@
 #include "Config/DEV_Config.h"
 #include "lcd/LCD_1in28.h"
 
-#define BLACK          0x0000
-#define BLUE           0x001F
-#define GREEN          0x07E0
-#define CYAN           0x7FFF
-#define RED            0xF800
-#define MAGENTA        0xF81F
-#define WHITE          0xFFFF
 const int colors[] = {BLACK, BLUE, GREEN, CYAN, RED, MAGENTA, WHITE};
+const int num_colors = (sizeof(colors)/sizeof(colors[0]));
 
 int main() {
-    uint16_t i = 0;
+    uint16_t color = 0;
     if (DEV_Module_Init() != 0)
         return -1;
 
-    adc_init();
-    adc_gpio_init(29);
-    adc_select_input(3);
     /* LCD Init */
     printf("1.28inch LCD demo...\r\n");
     LCD_1IN28_Init(HORIZONTAL);
@@ -27,13 +18,10 @@ int main() {
 
     printf("Starting to rotate through colors\n");
     while (true) {
-        LCD_1IN28_Clear(colors[i]);
-        sleep_ms(250);
-
-        i = (i+1) % (sizeof(colors)/sizeof(colors[0]));
-        printf("%d\n", i);
+        LCD_1IN28_Clear(colors[color]);
+        color = (color+1) % num_colors;
+        sleep_ms(500);
+        printf("color:%d\n", color);
     }
-
-    /* Module Exit */
     DEV_Module_Exit();
 }
