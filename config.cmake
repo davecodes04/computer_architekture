1f8e1903545c197eb6e704b3479048c94229d14a
Rainer Keller <rainer.keller@hs-esslingen.de>
2025-12-25 18:03:07 +0100

    This should include the bin/ in PATH

--- config.cmake
+++ config.cmake
@@ -6,4 +6,4 @@ set(PICO_BOARD "waveshare_rp2350_lcd_1.28") # or "waveshare_rp2040_lcd_1.28"
 # set(picotool_DIR "${CMAKE_CURRENT_LIST_DIR}../external/usr/picotool")
 set(PICOTOOL_FETCH_FROM_GIT_PATH "${CMAKE_CURRENT_LIST_DIR}/../external/picotool")
 
-cmake_path(SET PATH "${PROJECT_SOURCE_DIR}../external/usr/:${PATH}")
+cmake_path(SET PATH "${PROJECT_SOURCE_DIR}../external/usr/bin:${PATH}")
