b638a80a1c415a74ff29d63101964dc38a158cff
Rainer Keller <rainer.keller@hs-esslingen.de>
2026-04-09 10:47:31 +0200

    Add debugging of the GPIO state.

--- 02_gpio_joystick/src/02_gpio_joystick.c
+++ 02_gpio_joystick/src/02_gpio_joystick.c
@@ -8,6 +8,25 @@
 #define GPIO_Y      27
 #define GPIO_BUTTON 16
 
+/* BEGIN: JUST FOR DEBUGGING */
+struct state {
+    bool is_pulled_up;
+    bool is_pulled_down;
+    bool is_input_hysteresis_enabled;
+    enum gpio_drive_strength drive_strength;
+    enum gpio_slew_rate slew_rate;
+};
+
+void gpio_get_state(uint gpio, struct state * gpio_state) {
+    assert (NULL != gpio_state);
+    gpio_state->is_pulled_up = gpio_is_pulled_up(gpio);
+    gpio_state->is_pulled_down = gpio_is_pulled_down(gpio);
+    gpio_state->is_input_hysteresis_enabled = gpio_is_input_hysteresis_enabled (gpio);
+    gpio_state->drive_strength = gpio_get_drive_strength (gpio);
+    gpio_state->slew_rate = gpio_get_slew_rate (gpio);
+}
+/* END: JUST FOR DEBUGGING */
+
 int main() {
     stdio_init_all();
     adc_init();
@@ -17,6 +36,9 @@ int main() {
     gpio_init(GPIO_BUTTON);           // The GPIO connecting button of this joystick
     gpio_set_dir(GPIO_BUTTON, false); // Set as input
     gpio_pull_up(GPIO_BUTTON);        // Enable Pull-UP Resistor (inverts the button)
+
+    struct state input_state;
+    gpio_get_state (GPIO_BUTTON, &input_state);
 
     while (true)
     {
