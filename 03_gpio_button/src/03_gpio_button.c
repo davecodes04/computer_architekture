fec3bc7f22e6f99a234e91c7b3c2ee22b7915b00
Rainer Keller <rainer.keller@hs-esslingen.de>
2026-04-16 09:35:17 +0200

    Source should match the proposed bread-board.
    Rename functions so it's clear, they are not part of PICO-SDK,
    and mention WHERE the code for debugging the GPIO state was from.

--- 03_gpio_button/src/03_gpio_button.c
+++ 03_gpio_button/src/03_gpio_button.c
@@ -5,9 +5,9 @@
 
 #define GPIO_BUTTON 18
 
-#define WANT_PULL_DOWN_EXTERNAL
+// #define WANT_PULL_DOWN_EXTERNAL
 // #define WANT_PULL_DOWN
-// #define WANT_PULL_UP
+#define WANT_PULL_UP
 
 #if ((defined(WANT_PULL_DOWN_EXTERNAL) && (defined(WANT_PULL_DOWN) || defined(WANT_PULL_UP))) || \
      (defined(WANT_PULL_DOWN) && (defined(WANT_PULL_DOWN_EXTERNAL) || defined(WANT_PULL_UP))) || \
@@ -15,16 +15,29 @@
 #   error "ERROR: Should only define one of three pull-up/pull-down variants"
 #endif
 
+// Uncomment and define, if You want debugging of GPIO state
+#define WANT_DEBUG
+
 /*************** Function definitions ***********************************************/
 static void gpio_event_string(char *buf, uint32_t events);
-static void gpio_callback(uint gpio, uint32_t events);
-struct gpio_state;
-static void gpio_get_state(uint gpio, struct gpio_state * gpio_state);
+static void my_gpio_callback(uint gpio, uint32_t events);
+
+#ifdef WANT_DEBUG
+struct gpio_state;    // Forward declaration
+static void get_gpio_state(uint gpio, struct gpio_state * gpio_state);
+#endif /* WANT_DEBUG */
 /************************************************************************************/
-static const char *gpio_irq_str[];
+
+// See rp2350-datasheet.pdf (p. 604)
+static const char * gpio_irq_str[] = {
+        "LEVEL_LOW",  // 0x1
+        "LEVEL_HIGH", // 0x2
+        "EDGE_FALL",  // 0x4
+        "EDGE_RISE"   // 0x8
+};
 static char event_str[128];
 
-/* BEGIN: JUST FOR DEBUGGING */
+#ifdef WANT_DEBUG
 struct gpio_state {
     bool is_pulled_up;
     bool is_pulled_down;
@@ -33,7 +46,7 @@ struct gpio_state {
     enum gpio_slew_rate slew_rate;
 };
 
-static void gpio_get_state(uint gpio, struct gpio_state * gpio_state) {
+static void get_gpio_state(uint gpio, struct gpio_state * gpio_state) {
     assert (NULL != gpio_state);
     gpio_state->is_pulled_up = gpio_is_pulled_up(gpio);
     gpio_state->is_pulled_down = gpio_is_pulled_down(gpio);
@@ -41,7 +54,7 @@ static void gpio_get_state(uint gpio, struct gpio_state * gpio_state) {
     gpio_state->drive_strength = gpio_get_drive_strength (gpio);
     gpio_state->slew_rate = gpio_get_slew_rate (gpio);
 }
-/* END: JUST FOR DEBUGGING */
+#endif /* WANT_DEBUG */
 
 int main() {
     stdio_init_all();
@@ -57,23 +70,19 @@ int main() {
     gpio_pull_up(GPIO_BUTTON);
 #endif
 
+#ifdef WANT_DEBUG
     struct gpio_state input_state;
-    gpio_get_state (GPIO_BUTTON, &input_state);
+    get_gpio_state (GPIO_BUTTON, &input_state);
+#endif /* WANT_DEBUG */
 
-    gpio_set_irq_enabled_with_callback(GPIO_BUTTON, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(GPIO_BUTTON, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &my_gpio_callback);
 
     // Busy-loop forever
     while (true);
 }
 
-static const char *gpio_irq_str[] = {
-        "LEVEL_LOW",  // 0x1
-        "LEVEL_HIGH", // 0x2
-        "EDGE_FALL",  // 0x4
-        "EDGE_RISE"   // 0x8
-};
 
-static void gpio_callback(uint gpio, uint32_t events) {
+static void my_gpio_callback(uint gpio, uint32_t events) {
     gpio_event_string(event_str, events);
     static int button_press = 0;
 #if defined(WANT_PULL_DOWN_EXTERNAL) || defined(WANT_PULL_DOWN)
@@ -87,6 +96,17 @@ static void gpio_callback(uint gpio, uint32_t events) {
 }
 
 
+/**
+ * Translates the up to 4 GPIO IRQ Events into the string buf.
+ *
+ * Parses the 4 possible events using a Mask and "translates" those into
+ * human-readable String -- which needs to be large enough to possible 48
+ * characters (plus the NUL byte).
+ * See rp2350-datasheet.pdf (p. 604)
+ *
+ * @param[out] buf    Pointer to a string large enough to hold 4 event names (plus ", " and NUL)
+ * @param[in]  events The events provided to the Callback handler
+ */
 static void gpio_event_string(char *buf, uint32_t events) {
     for (uint i = 0; i < 4; i++) {
         uint mask = (1 << i);
